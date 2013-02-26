ae148003141749446edb9d18775b09708af17812
thilinatch <thilinach87@gmail.com>
2013-02-26 14:35:49 +0530

    Update test

--- MvcIntro/UnitTestProject1/UserControllerTest.cs
+++ MvcIntro/UnitTestProject1/UserControllerTest.cs
@@ -2,32 +2,30 @@
 using System.Linq;
 using System.Web.Mvc;
 using FluentAssertions;
-using MvcIntro.Models;
 using MvcIntro.Controllers;
+using MvcIntro.Models;
 using NUnit.Framework;
 using OpenQA.Selenium;
+using SimpleBrowser.WebDriver;
 
 namespace UnitTestProject1
 {
     [TestFixture]
     public class UserControllerTest
     {
-        private static UserRepo repo = new UserRepo();
-        private UserController controller = new UserController(repo);
+        private static readonly UserRepo repo = new UserRepo();
+        private readonly UserController controller = new UserController(repo);
 
-        [Test]
-        public void ShouldIncludeAddedUsers()
+        private List<User> ListUsers()
         {
-            User newUser = new User("Johnny", "USA");
-            repo.AddUser(newUser);
-
-            ListUsers().Any(p => p.Id == newUser.Id).Should().BeTrue();
+            var result = controller.Index() as ViewResult;
+            return (List<User>) result.Model;
         }
 
         [Test]
         public void SaveAllFields()
         {
-            User newUser = new User("Mike", "UK");
+            var newUser = new User("Mike", "UK");
             repo.AddUser(newUser);
 
             User createdUser = repo.GetUserById(newUser.Id);
@@ -36,9 +34,18 @@ namespace UnitTestProject1
         }
 
         [Test]
+        public void ShouldIncludeAddedUsers()
+        {
+            var newUser = new User("Johnny", "USA");
+            repo.AddUser(newUser);
+
+            ListUsers().Any(p => p.Id == newUser.Id).Should().BeTrue();
+        }
+
+        [Test]
         public void ShouldRemoveDeletedUsers()
         {
-            User userToBeDeleted = new User("Michelle", "Germany");
+            var userToBeDeleted = new User("Michelle", "Germany");
             repo.AddUser(userToBeDeleted);
 
             repo.DeleteUser(userToBeDeleted.Id);
@@ -46,30 +53,10 @@ namespace UnitTestProject1
             ListUsers().Any(p => p.Id == userToBeDeleted.Id).Should().BeFalse();
         }
 
-        private List<User> ListUsers()
-        {
-            ViewResult result = controller.Index() as ViewResult;
-            return (List<User>)result.Model;
-        }        
-
-        [Test]
-        public void UpdateAllFields()
-        {
-            User oldUser = new User("Wills", "Australia");
-            repo.AddUser(oldUser);
-            User newUser = new User("Alice", "Denmark");
-            repo.UpdateUser(oldUser.Id, newUser);
-
-            User updatedUser = repo.GetUserById(oldUser.Id);
-
-            updatedUser.Name.Should().Be(newUser.Name);
-            updatedUser.Address.Should().Be(newUser.Address);
-        }
-
         [Test]
         public void ShouldSavePerson()
         {
-            var browser = new SimpleBrowser.WebDriver.SimpleBrowserDriver();
+            var browser = new SimpleBrowserDriver();
 
             //Add user
             browser.Url = "http://localhost:54075/User/Create";
@@ -81,6 +68,20 @@ namespace UnitTestProject1
             browser.Url = "http://localhost:54075/User/Search";
             browser.FindElement(By.Id("SearchQuery")).SendKeys("darth vader");
             browser.FindElement(By.Id("search")).Submit();
+        }
+
+        [Test]
+        public void UpdateAllFields()
+        {
+            var oldUser = new User("Wills", "Australia");
+            repo.AddUser(oldUser);
+            var newUser = new User("Alice", "Denmark");
+            repo.UpdateUser(oldUser.Id, newUser);
+
+            User updatedUser = repo.GetUserById(oldUser.Id);
+
+            updatedUser.Name.Should().Be(newUser.Name);
+            updatedUser.Address.Should().Be(newUser.Address);
         }
     }
 }
