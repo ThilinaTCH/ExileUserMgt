702d4d5ec6b3a3a184a917a7aa179223e2aa871b
thilinatch <thilinach87@gmail.com>
2013-03-18 13:48:08 +0530

    Modify UI texts

--- MvcIntro/MvcIntro/Controllers/HomeController.cs
+++ MvcIntro/MvcIntro/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace MvcIntro.Controllers
 {
@@ -10,7 +6,7 @@ namespace MvcIntro.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ExileUserMgt web portal!";
+            ViewBag.Message = "Welcome to ExileContactMgt web portal!";
 
             return View();
         }
